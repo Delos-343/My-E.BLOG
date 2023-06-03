f322dfc9e416e2f15cbe0ed8abc73765692e1408
Delos-343 <mfachry.handoko@gmail.com>
2023-06-04 00:59:05 +0700

    rm experimental features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Layout } from '../components'
-import '../styles/global-styles.scss'
+import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
 
